6b7078256e7aedb0f464d69cb37d7c2d4c9c0450
Ping Chen <remotenonsense@gmail.com>
2019-11-03 11:08:12 +0900

    href is not removed on re-render

--- src/Example/Hello.purs
+++ src/Example/Hello.purs
@@ -3,30 +3,44 @@ module Example.Hello where
 import Example.Prelude
 
 import Data.Const (Const)
+import Data.Maybe (Maybe(..))
 import Effect.Aff (Aff)
 import Halogen as H
 import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
 
 type Query = Const Void
 
-type Action = Void
+data Action
+  = Toggle
 
 type State =
-  { value :: String
+  { editing :: Boolean
   }
 
 type HTML = H.ComponentHTML Action () Aff
 
 initialState :: State
 initialState =
-  { value: ""
+  { editing: true
   }
 
 render :: State -> HTML
 render state =
   HH.div
   [ class_ "text-base font-bold"]
-  [ HH.text "hello tailwind"
+  [ HH.a
+    ( if state.editing
+      then
+        []
+      else
+        [ HP.href "https://github.com"]
+    )
+    [ HH.text "hello" ]
+  , HH.button
+    [ HE.onClick $ Just <<< const Toggle ]
+    [ HH.text "toggle"]
   ]
 
 component :: H.Component HH.HTML Query Unit Void Aff
@@ -34,4 +48,9 @@ component = H.mkComponent
   { initialState: const initialState
   , render
   , eval: H.mkEval H.defaultEval
+      { handleAction = handleAction }
   }
+  where
+  handleAction = case _ of
+    Toggle ->
+      H.modify_ \s -> s { editing = not s.editing }
